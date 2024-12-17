a34b80a60e08bc39140d435c138f151df84d72f6
Jesus Arroyo <38357161+jsarroyo@users.noreply.github.com>
2024-12-16 23:23:36 -0600

    Merge

---
+++ SiteAsientos/SiteAsientos/Extensions/Session.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace SiteAsientos.Extensions
+{
+    public static class Session
+    {
+        //Guarda en sesion en formato json un usuario
+        public static void GuardarObjeto(this ISession sesion, string key, object value)
+        {
+            string data = JsonConvert.SerializeObject(value);
+            sesion.SetString(key, data);
+        }
+
+        //Devuelve el usuario en session
+        public static T ObtenerUsuario<T>(this ISession sesion, string key)
+        {
+            string data = sesion.GetString(key);
+            if (data == null)
+            {
+                return default(T);
+            }
+            else
+            {
+                return JsonConvert.DeserializeObject<T>(data);
+            }
+        }
+    }
+}
